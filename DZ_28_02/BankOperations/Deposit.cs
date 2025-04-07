6297b8ad390725d93cfa027afdbb5be6f4d000ca
ElNefro <131360806+ElNefro@users.noreply.github.com>
2025-04-07 13:37:55 +0500

    Add files via upload

---
+++ DZ_28_02/BankOperations/Deposit.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    public class Deposit
+    {
+        private string FIO;
+        private double sum;
+        private static double percent = 20;
+
+        public Deposit(string fio, double initialSum) 
+        {
+            FIO = fio;
+            sum = initialSum;
+        }
+
+        public double GetPercent()
+        {
+            return percent;
+        }
+
+        public double GetSum()
+        {
+            return sum;
+        }
+
+        public static Deposit operator ++(Deposit obj)
+        {
+            obj.sum += percent * obj.sum / 100;
+            return obj;
+        }
+
+        public static Deposit operator --(Deposit obj)
+        {
+            obj.sum -= percent * obj.sum / 100;
+            return obj;
+        }
+    }
+}
---
+++ DZ_28_02/Program.cs
@@ -0,0 +1,158 @@
+﻿
+// Перегрузка операторов
+
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DZ_28_02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Figure1.V(12.2));
+            Console.WriteLine(Figure1.V(12.22, 34.11, 99.6));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Number1.PrintMax(12, 10));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Number2.Multiply(-12, -10));
+            Console.WriteLine(Number2.Multiply(12.99, 10.11));
+            Console.WriteLine(Number2.Multiply(121212121212, 90));
+            Console.WriteLine(Number2.Multiply(0, 10));
+
+            Console.WriteLine("\n");
+
+            Student bob = new Student("Bob Peter Andersen", 3.5, 5, "15.07.2024");
+            Student sue = new Student("Sue Augustina Lorenzzo", 5, 3, "21.07.2024");
+            Student victor = new Student("Victor Petrovich Diakonov", 5, 5, "30.07.2024");
+
+            Console.WriteLine("Bob better Sue? " + (bob > sue));
+            Console.WriteLine("Victor better Sue? " + (victor > sue));
+            Console.WriteLine("Bob better Victor? " + (bob > victor));
+
+            Console.WriteLine("Bob has passed? " + (Admission.IsPass(bob)));
+            Console.WriteLine("Victor has passed? " + (Admission.IsPass(victor)));
+            Console.WriteLine("Sue has passed? " + (Admission.IsPass(sue)));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Figure2.Volume(12));
+            Console.WriteLine(Figure2.Volume(1, 5));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Figure3.S(9));
+            Console.WriteLine(Figure3.S(11, 33, 22));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Figure4.Square(7));
+            Console.WriteLine(Figure4.Square(5, 2));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Figure5.Sq(7));
+            Console.WriteLine(Figure5.Sq(5.2, 2.44));
+            Console.WriteLine(Figure5.Sq(4, 3));
+            Console.WriteLine(Figure5.Sq(9, 1, 7));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Figure6.P(7));
+            Console.WriteLine(Figure6.P(5.2, 2.44));
+            Console.WriteLine(Figure6.P(4.5, 3, 6));
+            Console.WriteLine(Figure6.P(9, 1, 7));
+
+            Console.WriteLine("\n");
+
+            int[] arr = { 34, 4, 1, 12, 10 };
+            Console.WriteLine(Array1.AverageNum(arr));
+
+            double[] arr2 = { 12.88, 9.9, 0.88, 77.11 };
+            Console.WriteLine(Array1.AverageNum(arr2));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(Array2.MaxNum(arr));
+            Console.WriteLine(Array2.MaxNum(arr2));
+
+            Console.WriteLine("\n");
+
+            Deposit deposit = new Deposit("Georgy Smith Sedric", 1000);
+            Console.WriteLine($"Sum before = {deposit.GetSum()}");
+            deposit++;
+            Console.WriteLine($"Sum after = {deposit.GetSum()}");
+
+            Console.WriteLine("\n");
+
+            Credit credit1 = new Credit("Georgy Smith Sedric", 1000);
+            Credit credit2 = new Credit("Sally Goodman Smith", 500);
+            Console.WriteLine($"Sum before = {credit1.GetSum()}");
+            Console.WriteLine($"Payment = {credit1.GetPercent()}");
+
+            Console.WriteLine($"Difference between sum and payment = {credit1.GetSum() - credit1.GetPercent()}");
+
+            Console.WriteLine("\n");
+
+            College c = new College("Top", 500, 20, 30);
+            College c2 = new College("Toppp", 700, 40, 50);
+            Console.WriteLine(c < c2);
+            Console.WriteLine(Standart.isFits(c));
+
+            Console.WriteLine("\n");
+
+            Cat barsik = new Cat("Barsik", "siam", "12.02.2020", 3);
+            Cat Tolik = new Cat("Tolik", "siam", "30.10.2017", 5);
+            Console.WriteLine(barsik > Tolik);
+            Console.WriteLine(barsik * Tolik);
+            Console.WriteLine(CatWeight.WhatShape(Tolik));
+
+            Console.WriteLine("\n");
+
+            Human kolan = new Human("Коля Петров Николаевич", "male", 16, "голубые");
+            Human anya = new Human("Анна Сергеевна Йомае", "female", 25, "карие");
+
+            Console.WriteLine(kolan + anya);
+            Console.WriteLine(Efficiency.IsEfficiency(kolan));
+            Console.WriteLine(Efficiency.IsEfficiency(anya));
+
+            Console.WriteLine("\n");
+
+            Game game = new Game("Skater 2", 2000, 10000, 8.6, 9.0, 2.4);
+            Game game2 = new Game("Skater 4", 1000, 5000, 6.7, 8.0, 1.0);
+            game++;
+            Console.WriteLine(PopularGame.IsGamePopular(game));
+            Console.WriteLine($"Какая игра лучше, Skater 2 или Skater 4? Ответ: {game > game2}");
+            Console.WriteLine($"Какая игра лучше, Skater 2 или Skater 4? Ответ: {game < game2}");
+            Console.WriteLine(PopularGame.IsGamePopular(game2));
+
+            Console.WriteLine("\n");
+
+            State us = new State(340000000, "USA", 9867000);
+            State germany = new State(82000000, "Germany", 357592);
+            Console.WriteLine(us > germany);
+            Console.WriteLine(us + germany);
+            Console.WriteLine(Empire.IsEmpire(germany));
+
+            Console.WriteLine("\n");
+
+            Ingredient ingredient1 = new Ingredient("Морковь", "огонь", 10);
+            Ingredient ingredient2 = new Ingredient("Ноготь носорога", "защита от огня", 300);
+            Console.WriteLine(ingredient1 + ingredient2);
+            Console.WriteLine(Price.WhichPrice(ingredient1));
+
+            Console.WriteLine("\n");
+
+            Post post1 = new Post(1212, 100, 20, "hello");
+            Post post2 = new Post(1213, 150, 100, "bye");
+            Console.WriteLine(post2 + 12);
+            Console.WriteLine(post2 - 12);
+            Console.WriteLine(Recomendations.Recommend(post2));
+
+        }
+    }
+}
